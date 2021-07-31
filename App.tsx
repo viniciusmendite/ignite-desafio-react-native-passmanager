ca3727b1dc388df19882375b466e8584f28dfafc
Vinicius Mendite <vinicius@sharbe.com.br>
2021-07-30 22:07:19 -0300

    create global theme

--- App.tsx
+++ App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import { NavigationContainer } from '@react-navigation/native';
+import { ThemeProvider } from 'styled-components';
 import Toast from 'react-native-toast-message';
 
 import { AppRoutes } from './src/routes/app.routes';
 import { StorageDataProvider } from './src/contexts/StorageDataContext';
+import theme from './src/global/styles/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,7 +22,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <StorageDataProvider>
-        <AppRoutes />
+        <ThemeProvider theme={theme}>
+          <AppRoutes />
+        </ThemeProvider>
       </StorageDataProvider>
       <Toast ref={(ref) => Toast.setRef(ref)} />
     </NavigationContainer>
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: '#4E3975',
+    text_empty: '#9883BF',
+    shape: '#ffffff'
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+  }
+}
